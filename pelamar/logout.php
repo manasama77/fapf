ed3b39e5cff529152ddba49ea085be082196c0a8
Adam PM <adam.pm77@gmail.com>
2023-05-18 22:48:05 +0700

    update logout

--- pelamar/logout.php
+++ pelamar/logout.php
@@ -3,4 +3,4 @@ session_start();
 session_destroy();
 require_once('../constants.php');
 
-return header("Location:" . APP_URL . '/pelamar/login.php');
+return header("Location:" . APP_URL . '/login.php');
